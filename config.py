2eed61b404bc50f7c1ad294e885217ceb44c1174
YouKnowWho117 <163604324+YouKnowWho117@users.noreply.github.com>
2024-03-19 20:35:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ mysql_host = os.environ.get("mysql_host")
 
 class config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'fudi'
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:3306/doodle'
+        SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:3306/doodle'
         SQLALCHEMY_TRACK_MODIFICATIONS = False
