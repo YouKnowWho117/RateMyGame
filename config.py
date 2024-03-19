c9133da80a21a196bf819a3f1dcc2ac25f53bc6a
YouKnowWho117 <163604324+YouKnowWho117@users.noreply.github.com>
2024-03-19 20:06:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+mysql_user = os.environ.get("mysql_user")
+mysql_password = os.environ.get("mysql_password")
+mysql_host = os.environ.get("mysql_host")
+
 
 class config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'fudi'
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://flaskdblz:00sJ%Z!m5&80IMQZ^TK!7K!3ps*8RdDu@dbflasklz.mysql.database.azure.com:3306/doodle'
+        'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:3306/doodle'
         SQLALCHEMY_TRACK_MODIFICATIONS = False
